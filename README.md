bb010ed6a4096dcc83b3852903bea1618396c845
Vaughn Kottler <vkottler@wisc.edu>
2017-02-20 03:37:06 -0600

    let's do this

--- README.md
+++ README.md
@@ -2,5 +2,22 @@
 
 *Contributors: Vaughn Kottler*
 
-Source code for our website.
+## Local Development
 
+Install [Git](https://git-scm.com/). Make a new folder for the project, open `git bash` and navigate to this folder. Install [node.js LTS](https://nodejs.org/en/).
+
+Run `git init` followed by `git remote add origin https://github.com/vkottler/badgerloopSoftware.git` followed by `git pull origin master`.
+
+Run `npm install` while in the same folder. Now, you can run `node server website` or `node server dashboard` to serve local web traffic for either of these pages with a simple [express.js](http://expressjs.com/) server.
+
+## Website Technologies
+
+1. **Angular? Bootstrap?**
+
+## Dashboard Technologies
+
+1. **Angular? Bootstrap?**
+
+## Server and Hosting
+
+1. **Ubuntu Server 16.04 LTS**
---
+++ server.js
@@ -0,0 +1,49 @@
+/************************************************************************************/
+/*                              Server Initialization                               */
+/************************************************************************************/
+express = require('express');
+//bodyParser = require('body-parser');
+http = require('http');
+port = 8080;
+app = express();
+server = http.createServer(app).listen(port, function() {});
+var folder = __dirname + '/web';
+var file;
+
+if (process.argv.length != 3) startupError('have the right number of arguments');
+switch (process.argv[2].toLowerCase()) {
+  case 'd':
+  case 'dash':
+  case 'dashboard': 
+    file = 'dashboard.html';
+    console.log("starting dashboard locally on port " + port);
+    break;
+
+  case 'w':
+  case 'web':
+  case 'website':
+    file = 'index.html';
+    console.log("starting website locally on port " + port);
+    break;
+
+  default:
+    startupError('give a proper argument');
+}
+
+function startupError(problem) {
+  console.log('[ERROR] usage: node start [client | server] you didn\'t ' + problem);
+  process.exit();
+}
+
+app.use('/', express.static(folder, {"index": file}));
+/************************************************************************************/
+/************************************************************************************/
+
+
+/************************************************************************************/
+/*                                API Testing                                       */
+/************************************************************************************/
+//app.use(bodyParser.json());
+
+/************************************************************************************/
+/************************************************************************************/
