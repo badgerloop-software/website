5fdaf046fd4147a3fa523c6ebf249f24e6aa7f63
Vaughn Kottler <vaughnkottler@gmail.com>
2017-03-06 23:45:52 +0000

    Pod page attempt

--- web/js/controllers/pod.js
+++ web/js/controllers/pod.js
@@ -1,4 +1,11 @@
 angular.module('controllers')
-.controller('podController', function($scope) {
-    $scope.test = "POD_PAGE";
+.controller('MainCtrl', function($scope, $location, $anchorScroll, $routeParams) {
+  $scope.scrollTo = function(id) {
+    $location.hash(id);
+    $anchorScroll();
+  }
 });
+
+//.controller('podController', function($scope) {
+//    $scope.test = "POD_PAGE";
+//});
