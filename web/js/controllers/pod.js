9e66224b37829878ac014a088e564a9a81c565f3
Vaughn Kottler <vaughnkottler@gmail.com>
2017-03-07 00:03:16 +0000

    revert pod page

--- web/js/controllers/pod.js
+++ web/js/controllers/pod.js
@@ -1,11 +1,4 @@
 angular.module('controllers')
-.controller('MainCtrl', function($scope, $location, $anchorScroll, $routeParams) {
-  $scope.scrollTo = function(id) {
-    $location.hash(id);
-    $anchorScroll();
-  }
+.controller('podController', function($scope) {
+    $scope.test = "POD_PAGE";
 });
-
-//.controller('podController', function($scope) {
-//    $scope.test = "POD_PAGE";
-//});
