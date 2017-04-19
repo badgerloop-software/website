82b223835e5c53dc983ea0d73a634e8b316249de
Vaughn Kottler <vkottler@wisc.edu>
2017-04-19 13:05:32 -0500

    minifying and re-styling sponsorship page

--- web/js/controllers/sponsorship.js
+++ web/js/controllers/sponsorship.js
@@ -1,140 +1,116 @@
 var platinum_logos = [
 	{
-		name: 'Altium',
-		img: 'Altium',
+		name: 'Altium', img: 'Altium',
 		site: 'http://www.altium.com/'
 	},
 	{
-		name: 'ANSYS',
-		img: 'ANSYS',
+		name: 'ANSYS', img: 'ANSYS',
 		site: 'http://www.ansys.com/'
 	},
 	/*{
-		name: 'Tektronix', //thisn
-		img: 'Tektronix',
+		name: 'Tektronix', img: 'Tektronix',
 		site: 'http://www.tek.com/'
 	},*/
 	{
-		name: 'HP',
-		img: 'HP',
+		name: 'HP', img: 'HP',
 		site: 'http://www.hp.com/country/us/en/welcome.html'
 	},
 	/*{
-		name: 'OnFloor', //thisn
-		img: 'OnFloor',
+		name: 'OnFloor', img: 'OnFloor',
 		site: 'https://onfloor.com/'
 	},*/
-]
-var gold_logos= [
+];
+
+var gold_logos = [
 	{
-		name: 'Siemens',
-		img: 'Siemens',
+		name: 'Siemens', img: 'Siemens',
 		site: 'http://www.siemens.com/us/en/home.html'
 	},
-]
+];
 
-var silver_logos=[
+var silver_logos = [
 	{
-		name: '@1403',
-		img: '1403',
+		name: '@1403', img: '1403',
 		site: 'http://universityresearchpark.org/the-property/1403-2/'
 	},
 	{
-		name: 'Boxx',
-		img: 'Boxx',
+		name: 'Boxx', img: 'Boxx',
 		site: 'https://www.boxx.com/'
 	},
 	{
-		name: 'Carrier Web',
-		img: 'Carrier-Web',
+		name: 'Carrier Web', img: 'Carrier-Web',
 		site: 'https://www.carrierweb.com/'
 
 	},
 	{
-		name: 'Plexus',
-		img: 'Plexus',
+		name: 'Plexus', img: 'Plexus',
 		site: 'http://www.plexus.com/'
 	},
 	{
-		name: 'Qualcomm',
-		img: 'Qualcomm',
+		name: 'Qualcomm', img: 'Qualcomm',
 		site: 'https://www.qualcomm.com/'
 	},
 	{
-		name: 'Snap-on',
-		img: 'Snap-on',
+		name: 'Snap-on', img: 'Snap-on',
 		site: 'https://store.snapon.com/'
 	},
-]
+];
 
-var bronze_logos=[
+var bronze_logos = [
 	/*{
-		name: 'Keysight Technologies', //thisn
-		img: 'Keysight_Technologies',
+		name: 'Keysight Technologies', img: 'Keysight_Technologies',
 		site: 'http://www.keysight.com/main/home.jspx?cc=US&lc=eng'
 	},*/
 	{
-		name: 'ATA Engineering',
-		img: 'ATA_Engineering',
+		name: 'ATA Engineering', img: 'ATA_Engineering',
 		site: 'http://www.ata-e.com/'
 	},
 	{
-		name: 'AWC Wire',
-		img: 'AWC',
+		name: 'AWC Wire', img: 'AWC',
 		site: 'http://www.awcwire.com/'
 	},
 	{
-		name: 'Bimba',
-		img: 'Bimba',
+		name: 'Bimba', img: 'Bimba',
 		site: 'http://www.bimba.com/'
 	},
 	{
-		name: 'Bulletproof Automotive',
-		img: 'Bulletproof_Automotive',
+		name: 'Bulletproof Automotive', img: 'Bulletproof_Automotive',
 		site: 'http://www.bulletproofautomotive.com/'
 	},
 	{
-		name: 'Derksen',
-		img: 'Derksen',
+		name: 'Derksen', img: 'Derksen',
 		site: 'http://www.derksenprinters.com/'
 	},
 	{
-		name: 'Design Concepts',
-		img: 'Design-Concepts',
+		name: 'Design Concepts', img: 'Design-Concepts',
 		site: 'http://www.design-concepts.com/'
 	},
 	{
-		name: 'E-Switch',
-		img: 'ESwitch',
+		name: 'E-Switch', img: 'ESwitch',
 		site: 'https://www.e-switch.com/'
 	},
 	/*{
-		name: 'HomeRevolution', //thisn
-		img: 'HomeRevolution',
+		name: 'HomeRevolution', img: 'HomeRevolution',
 		site: 'https://www.homerev.com/'
 	},*/
 	{
-		name: 'Plymouth Foam',
-		img: 'Plymouth_Foam',
+		name: 'Plymouth Foam', img: 'Plymouth_Foam',
 		site: 'http://www.plymouthfoam.com/'
 	},
 	{
-		name: 'Triad Magnetics',
-		img: 'Triad',
+		name: 'Triad Magnetics', img: 'Triad',
 		site: 'http://www.triadmagnetics.com/index.html'
 	},
-]
-
-
+];
 
 angular.module('controllers')
 .controller('sponsorshipController', function($scope) {
-	$scope.header = "Thanks to our sponsors!";
-	$scope.platinum_logos = platinum_logos;
-	$scope.gold_logos = gold_logos;
-	$scope.silver_logos = silver_logos;
-	$scope.bronze_logos = bronze_logos;
-	$scope.logos = [platinum_logos, gold_logos, silver_logos, bronze_logos]
+	$scope.platinum_logos =	{ title: "Platinum", array: platinum_logos };
+	$scope.gold_logos = 	{ title: "Gold", array: gold_logos };
+	$scope.silver_logos =	{ title: "Silver", array: silver_logos };
+	$scope.bronze_logos =	{ title: "Bronze", array: bronze_logos };
+
+	$scope.logos = [$scope.platinum_logos, $scope.gold_logos, $scope.silver_logos, $scope.bronze_logos];
 
 	if (globalUpdateButtons) globalUpdateButtons();
 });
