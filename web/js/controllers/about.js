db3e74267486507c69f25daf336ec85f93dfdd4a
Kody Fisher <kodyfisher15@gmail.com>
2017-04-23 00:00:18 -0500

    Working on team page popup

--- web/js/controllers/about.js
+++ web/js/controllers/about.js
@@ -1,5 +1,32 @@
 angular.module('controllers')
 .controller('aboutController', function($scope) {
 
+$scope.factoids = [
+  {
+    fact:"Competition II Dates: ",
+    answer: "August 25th - August 27th, 2017",
+  },
+  {
+    fact:"Host: ",
+    answer: "SpaceX",
+  },
+  {
+    fact:"Location: ",
+    answer: "SpaceX's Headquarters in Hawthorne, California",
+  },
+  {
+    fact:"Number of Teams: ",
+    answer: "22",
+  },
+  {
+    fact:"Where are the Teams From? ",
+    answer: "16 from the United States, one from Poland, two from Germany, one from the United Kingdom, one from India, one from Japan",
+  }, // this needs to be checked, I don't think it includes rLoop
+  {
+    fact:"Objective of the Competition: ",
+    answer: "Maximum Speed",
+  },
+];
+
   if (globalUpdateButtons) globalUpdateButtons();
 });
--- web/js/controllers/team.js
+++ web/js/controllers/team.js
@@ -313,5 +313,42 @@ email: "mcheadle@wisc.edu",
 		feasibility, faculty_advisor
 	];
 
+
+function showPanel($event) {
+	var panelPosition = $mdPanel.newPanelPosition()
+			.absolute()
+			.top('50%')
+			.left('50%');
+
+	var panelAnimation = $mdPanel.newPanelAnimation()
+			.targetEvent($event)
+			.defaultAnimation('md-panel-animate-fly')
+			.closeTo('.show-button');
+
+	var config = {
+		attachTo: angular.element(document.body),
+		controller: DialogController,
+		controllerAs: 'ctrl',
+		position: panelPosition,
+		animation: panelAnimation,
+		targetEvent: $event,
+		templateUrl: 'dialog-template.html',
+		clickOutsideToClose: true,
+		escapeToClose: true,
+		focusOnOpen: true
+	}
+
+	$mdPanel.open(config)
+			.then(function(result) {
+				panelRef = result;
+			});
+}
+
+function DialogController(MdPanelRef) {
+	function closeDialog() {
+		if (MdPanelRef) MdPanelRef.close();
+	}
+}
+
   if (globalUpdateButtons) globalUpdateButtons();
 });
