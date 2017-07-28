5dd33275dee0693f844641fad28523d7dd0d6005
kfish15 <kodyfisher15@gmail.com>
2017-07-27 19:51:02 -0500

    About page updates, new fun facts added

--- web/js/controllers/about.js
+++ web/js/controllers/about.js
@@ -3,31 +3,31 @@ angular.module('controllers')
 
 $scope.factoids = [
   {
-    fact:"Competition II Date: ",
+    fact:"When is Competition II? ",
     answer: "August 27th, 2017",
   },
   {
-    fact:"Host of Competition II: ",
+    fact:"Who hosts Competition II? ",
     answer: "SpaceX",
   },
   {
-    fact:"Location of the competition: ",
+    fact:"Where is the competition held? ",
     answer: "Hawthorne, California at SpaceX's Headquarters",
   },
   {
-    fact:"Number of Teams at Competition II: ",
+    fact:"How many teams will be at Competition II? ",
     answer: "24",
   },
   {
-    fact:"Objective of the Competition: ",
-    answer: "Maximum Speed",
+    fact:"What is the objective of Competition II? ",
+    answer: "Maximum Speed. Fastest pod wins!",
   },
   {
     fact: "Will the event be livestreamed? ",
     answer: " You bet! We will post the link as the event draws closer."
   },
   {
-    fact: "Length of the Vacuum Tube: ",
+    fact: "What is the length of the vacuum tube? ",
     answer: "3/4ths of a mile (3960 feet)"
   },
   {
@@ -36,7 +36,15 @@ $scope.factoids = [
   },
   {
     fact: "How much did it cost to build Pod II? ",
-    answer: "Roughly $40,000 "
+    answer: "Roughly $40,000"
+  },
+  {
+    fact: "How fast do we expect to go? ",
+    answer: "Roughly 223 MPH (100 m/s)"
+  },
+  {
+    fact: "How many competitions have we been in? ",
+    answer: "Two! We took third at Design Weekend (January 2015), won the innovation award at Competition I (January 2016) and look forward to Competition II."
   },
 ];
 
